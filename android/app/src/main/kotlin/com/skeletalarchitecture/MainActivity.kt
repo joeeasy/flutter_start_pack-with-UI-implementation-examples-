5aca4fc9af5f8c5129b2a39dad4058e75580ecad
joeeasy <jehonadabokpus@gmail.com>
2020-06-29 23:11:24 +0100

    initial project setup with stacked

---
+++ android/app/src/main/kotlin/com/skeletalarchitecture/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.skeletalarchitecture
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:skeletalarchitecture/ui/view/home/home_view.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Provider archicture with stack',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeView(),
+    );
+  }
+}
